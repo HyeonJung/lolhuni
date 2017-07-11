ba5cd4cafd837ce3983d08775ca35bd586832f37
현중 <yhhj1592@gmail.com>
2017-07-11 10:47:00 +0900

    1분마다 체크하도록 수정

--- src/main/java/com/hj/lolhuni/schedule/ScheduledTasks.java
+++ src/main/java/com/hj/lolhuni/schedule/ScheduledTasks.java
@@ -47,73 +47,9 @@ public class ScheduledTasks {
 	 
 	 @Autowired
 	 GameService gameService;
-	 /**
-	  * 5분마다 게임 체크
-	  */
-//	 @Scheduled(fixedRate = 60000)
-//	 public void checkCurrentGame() {
-//		 Summoner summoner = lolService.getSummonerInfo("고릴라피시방");
-//		 CurrentGameInfo gameInfo = null;
-//		 
-//		 if (summoner != null) {
-//			 gameInfo = lolService.getGameInfo(summoner.getId());
-//		 }
-//		 
-//		 if (summoner == null || gameInfo == null) {
-//			 logger.debug("### {}님은 현재 게임 중이 아닙니다.",summoner.getName());
-//			 
-//		 } else {
-//			 logger.debug("### {}님은 현재 게임 중입니다.",summoner.getName());
-//			 
-//			 if (gameInfo.getGameLength() > 0 && gameInfo.getGameLength() < 60) {
-//				 lolService.sendFbMessage("고릴라피시방님은 현재 게임 중입니다.", "+82(010)2517-1592");
-//				 lolService.sendFbMessage("고릴라피시방님은 현재 게임 중입니다.", "+82(010)4140-8702");
-//				 lolService.sendFbMessage("와 유동훈 혼자 겜 쳐하네;;", "+82(010)3632-7381");
-//			 }
-//		 }
-//	 }
-	 
-//	 /**
-//	  * 5분마다 게임 체크 (No DB)
-//	  */
-//	 @Scheduled(fixedRate = 300000)
-//	 public void checkCurrentGameMinute() {
-//		
-//		CurrentGameInfo gameInfo = null;
-//		for (CheckList list : CheckList.values()) {
-//			 gameInfo = lolService.getGameInfo(list.getId());
-//			 if (gameInfo == null) {
-//				 logger.debug("### {}님은 현재 게임 중이 아닙니다.", list.name());
-//			 } else {
-//				
-//				 String championName = "";
-//				 
-//				 for (CurrentGameParticipant participant : gameInfo.getParticipants()) {
-//					 if (participant.getSummonerId() == list.getId()) {
-//						 String champId = "champ" + participant.getChampionId();
-//						 logger.debug("### champId = {}",champId);
-//						 ChampionInfo championInfo = ChampionInfo.valueOf(champId);
-//						 championName = championInfo.getChampionName();
-//					 }
-//				 }
-//				 logger.debug("### {}님은 현재 {}(으)로 게임 중입니다.",list.name(),championName);
-//				 
-//				 if (gameInfo.getGameLength() > 0 && gameInfo.getGameLength() < 300) {
-//					 for (PhoneNumList numList : PhoneNumList.values()) {
-//						 
-//						 if (!(numList.name().equals(list.name()))) {
-//							 //lolService.sendFbMessage(list.name() + "님은 현재 " + championName + "(으)로 게임 중입니다.", numList.getTel());
-//						 }
-//					 }
-//				 }
-//		     }
-//		}
-//		
-//		logger.debug("==============================================");
-//	 }
 	 
 	 /**
-	  * 5분마다 게임 체크 (db)
+	  * 1분마다 게임 체크 (db)
 	  */
 	 @Scheduled(fixedRate = 60000)
 	 public void checkCurrentGame() {
@@ -183,7 +119,5 @@ public class ScheduledTasks {
 				}
 			}
 		}
-		
-		logger.debug("==============================================");
 	 }
 }
