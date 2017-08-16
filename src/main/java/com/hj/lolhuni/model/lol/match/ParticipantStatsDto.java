69220fe135fee6882486885312b066dbe7a0c7de
윤현중 <guswnd1592@gaeasoft.co.kr>
2017-08-16 16:56:28 +0900

    api 변경으로 인한 수정

--- src/main/java/com/hj/lolhuni/model/lol/match/ParticipantStatsDto.java
+++ src/main/java/com/hj/lolhuni/model/lol/match/ParticipantStatsDto.java
@@ -74,6 +74,17 @@ public class ParticipantStatsDto {
 	private int killingSprees;
 	private long timeCCingOthers;
 	private long physicalDamageTaken;
+	private int playerScore0;
+	private int playerScore1;
+	private int playerScore2;
+	private int playerScore3;
+	private int playerScore4;
+	private int playerScore5;
+	private int playerScore6;
+	private int playerScore7;
+	private int playerScore8;
+	private int playerScore9;
+	
 	public long getPhysicalDamageDealt() {
 		return physicalDamageDealt;
 	}
@@ -148,6 +159,66 @@ public class ParticipantStatsDto {
 	}
 	public long getDamageDealtToObjectives() {
 		return damageDealtToObjectives;
+	}
+	public int getPlayerScore0() {
+		return playerScore0;
+	}
+	public void setPlayerScore0(int playerScore0) {
+		this.playerScore0 = playerScore0;
+	}
+	public int getPlayerScore1() {
+		return playerScore1;
+	}
+	public void setPlayerScore1(int playerScore1) {
+		this.playerScore1 = playerScore1;
+	}
+	public int getPlayerScore2() {
+		return playerScore2;
+	}
+	public void setPlayerScore2(int playerScore2) {
+		this.playerScore2 = playerScore2;
+	}
+	public int getPlayerScore3() {
+		return playerScore3;
+	}
+	public void setPlayerScore3(int playerScore3) {
+		this.playerScore3 = playerScore3;
+	}
+	public int getPlayerScore4() {
+		return playerScore4;
+	}
+	public void setPlayerScore4(int playerScore4) {
+		this.playerScore4 = playerScore4;
+	}
+	public int getPlayerScore5() {
+		return playerScore5;
+	}
+	public void setPlayerScore5(int playerScore5) {
+		this.playerScore5 = playerScore5;
+	}
+	public int getPlayerScore6() {
+		return playerScore6;
+	}
+	public void setPlayerScore6(int playerScore6) {
+		this.playerScore6 = playerScore6;
+	}
+	public int getPlayerScore7() {
+		return playerScore7;
+	}
+	public void setPlayerScore7(int playerScore7) {
+		this.playerScore7 = playerScore7;
+	}
+	public int getPlayerScore8() {
+		return playerScore8;
+	}
+	public void setPlayerScore8(int playerScore8) {
+		this.playerScore8 = playerScore8;
+	}
+	public int getPlayerScore9() {
+		return playerScore9;
+	}
+	public void setPlayerScore9(int playerScore9) {
+		this.playerScore9 = playerScore9;
 	}
 	public void setDamageDealtToObjectives(long damageDealtToObjectives) {
 		this.damageDealtToObjectives = damageDealtToObjectives;
